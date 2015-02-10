a6d3769569633df10ae8274fe8843f053670911b
boez <boez7878@gmail.com>
2015-02-11 01:03:57 +0300

    Second

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset =utf-8"/>
+<title>Страница сайта</title>
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<?php
+$t=1500;
+$f= 32;
+$g=35;
+echo ($f * $g);
+
+?>
+</body>
+</html>
+
